1fa3b1a7cddf3d306068c7e7bca940f78724f3ff
Rodrigo Arrieta Candia <riarrieta@uc.cl>
2023-05-10 12:11:23 -0400

    finish lotka_volterra_noisy

---
+++ examples/lotka_volterra_noisy/lotka_volterra.jl
@@ -0,0 +1,108 @@
+using LinearAlgebra
+using Flux
+using OrdinaryDiffEq
+using Plots
+using NeuralODEProject
+const NO = NeuralODEProject
+
+## Generate data
+function ftrue(du,u)
+    # lotka_volterra
+    # period = 2π/sqrt(α*γ)
+    # prey: x 
+    # predator: y 
+    x, y = u
+    α, β, δ, γ = (2f0/3f0, 4f0/3f0, 1.0f0, 1.0f0)
+    du[1] = dx = (α - β*y)x
+    du[2] = dy = (δ*x - γ)y
+  end
+u0 = Float32[1.0,1.0]
+tarray  = 0.0f0:0.25f0:10f0
+noise_σ = 0.1f0
+odelist = [NO.ODEStruct(ftrue,u0,tarray,noise_σ) for _ in 1:1]
+
+## Neural network
+nn = Chain(Dense(2 => 64,tanh),
+           Dense(64 => 2)) |> NO.NeuralODE
+## save
+@save joinpath(@__DIR__,"data.bson") nn
+## load
+@load joinpath(@__DIR__,"data.bson") nn
+## Optimiser
+optim = Flux.Adam(1e-2)  # Optimiser
+## Training v1
+costlist = zeros(length(odelist))
+ufunclist = Vector(undef,length(odelist))
+for _ in 1:300
+    for (i,ode_) in enumerate(odelist)
+        ufunc,cost = NO.node_train!(nn,ode_,optim)
+        ufunclist[i] = ufunc
+        costlist[i] = cost
+    end
+    @show maximum(costlist)
+end
+
+## Plot savefig(joinpath(@__DIR__,"fig.png"))
+#index = 5
+ode = odelist[1]
+ufunc = NO.forward_pass(nn,ode.u0,(first(tarray),last(tarray)))
+obs1,obs2 = zip(ode.utrue...)
+obs1,obs2 = collect(obs1),collect(obs2)
+
+plot(ode.soltrue,label=["Ground truth prey" "Ground truth predator"],color=:green,dpi=600,legend=:topleft)
+plot!(ufunc,label=["Prediction prey" "Prediction predator"],linestyle=:dash,color=:blue)
+scatter!(tarray,obs1,label="Observation prey",color=:green)
+scatter!(tarray,obs2,label="Observation predator",color=:green)
+xlabel!("Time")
+
+## Extrapolation
+tinf,tsup = 0f0,50f0
+
+tspan2 = (0.0f0, tsup)
+ufunc2 = NO.forward_pass(nn,u0,tspan2)
+soltrue2 = NO.ode_forward_pass(ftrue,u0,tspan2)
+
+tspan3 = (0.0f0, tinf)
+ufunc3 = NO.forward_pass(nn,u0,tspan3)
+soltrue3 = NO.ode_forward_pass(ftrue,u0,tspan3)
+
+## Plot savefig(joinpath(@__DIR__,"fig.png"))
+plot(soltrue3,label=["Ground truth prey" "Ground truth predator"],color=:green,legend=:bottomright,dpi=600)
+plot!(ufunc3,label=["Prediction prey" "Prediction predator"],color=:blue,linestyle=:dash)
+plot!(soltrue2,label="",color=:green)
+plot!(ufunc2,label="",color=:blue,linestyle=:dash)
+scatter!(tarray,obs1,label="Observation prey",color=:green)
+scatter!(tarray,obs2,label="Observation predator",color=:green)
+xlims!(tinf,tsup)
+xlabel!("Time")
+
+## Compare nn forward map angle
+t = range(0,1,100)
+r = 3
+u = [[r*cos(2π*t),r*sin(2π*t)] for t in t]
+dutrue = copy(u)
+for i in eachindex(dutrue)
+    ftrue(dutrue[i],dutrue[i])
+end
+dunn = [nn(u) for u in u]
+
+plot(t,[du[1] for du in dutrue],label="Ground truth u[1]",color=:green,legend=false)
+plot!(t,[du[2] for du in dutrue],label="Ground truth u[2]",color=:green)
+plot!(t,[du[1] for du in dunn],label="Prediction u[1]",color=:blue,linestyle=:dash)
+plot!(t,[du[2] for du in dunn],label="Prediction u[2]",color=:blue,linestyle=:dash)
+
+## Compare nn forward map radius
+t = range(0,1,100)
+r = 1
+ui = randn32(2)
+u = [r*ui*t for t in t]
+dutrue = copy(u)
+for i in eachindex(dutrue)
+    ftrue(dutrue[i],dutrue[i])
+end
+dunn = [nn(u) for u in u]
+
+plot(t,[du[1] for du in dutrue],label="Ground truth u[1]",color=:green,legend=false)
+plot!(t,[du[2] for du in dutrue],label="Ground truth u[2]",color=:green)
+plot!(t,[du[1] for du in dunn],label="Prediction u[1]",color=:blue,linestyle=:dash)
+plot!(t,[du[2] for du in dunn],label="Prediction u[2]",color=:blue,linestyle=:dash)
--- src/odestruct.jl
+++ src/odestruct.jl
@@ -7,13 +7,19 @@ struct ODEStruct
 end
 Base.show(io::IO,ode::ODEStruct) = print(io,"ODEStruct()")
 
-function ODEStruct(f,u0,tarray;odesolver=DEFAULT_ODE_SOLVER)
+function ODEStruct(f,u0,tarray,noise=0f0;odesolver=DEFAULT_ODE_SOLVER)
     ftrue(du,u,p,t) = f(du,u)
     tspan = (first(tarray), last(tarray))
     probtrue = ODEProblem(ftrue, u0, tspan)
     soltrue = odesolver(probtrue; saveat=tarray)
     @assert tarray == soltrue.t
     utrue = soltrue.u 
+    # add noise
+    if !iszero(noise)
+        T = eltype(eltype(utrue))
+        s = size(first(utrue))
+        utrue = copy(utrue) + [noise*randn(T,s) for _ in utrue]
+    end
     return ODEStruct(f,u0,tarray,soltrue,utrue)
 end
 
