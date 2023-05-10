c24538ab344636bccad15425e86e88e116a8a13d
Rodrigo Arrieta Candia <riarrieta@uc.cl>
2023-05-10 02:21:41 -0400

    linear example ready

--- examples/linear/linear.jl
+++ examples/linear/linear.jl
@@ -3,25 +3,31 @@ using Flux
 using OrdinaryDiffEq
 using Plots
 using NeuralODEProject
+using BSON: @save,@load
 const NO = NeuralODEProject
 
 ## Generate data
-const A = [-0.1f0 2.0f0; -2.0f0 -0.1f0]
+A = [-0.1f0 2.0f0; -2.0f0 -0.1f0]
 ftrue(du,u) = mul!(du,A,u)  # du = A*u
-tarray  = 0.0f0:0.1f0:1.0f0
-σ = 5 
-odelist = [NO.ODEStruct(ftrue,σ*randn32(2),tarray) for _ in 1:4]
+tarray  = 0.0f0:0.1f0:1f0
+ntraining = 4
+σ = 4
+odelist = [NO.ODEStruct(ftrue,σ*randn32(2),tarray) for _ in 1:ntraining]
+append!(odelist,[NO.ODEStruct(ftrue,σ*randn32(2),tarray) for _ in 1:ntraining])
 
 ## Neural network
-nn = Chain(Dense(2 => 50,tanh),
-           Dense(50 => 2)) |> NO.NeuralODE
-#optim = Flux.Descent(1e-2)  # Optimiser
+nn = Chain(Dense(2 => 64,tanh),
+           Dense(64 => 2)) |> NO.NeuralODE
+## save
+@save joinpath(@__DIR__,"data.bson") nn
+## load
+@load joinpath(@__DIR__,"linear.bson") nn
+## Optimiser
 optim = Flux.Adam(1e-2)  # Optimiser
-
 ## Training v1
 costlist = zeros(length(odelist))
 ufunclist = Vector(undef,length(odelist))
-for _ in 1:300
+for _ in 1:50
     for (i,ode_) in enumerate(odelist)
         ufunc,cost = NO.node_train!(nn,ode_,optim)
         ufunclist[i] = ufunc
@@ -36,18 +42,30 @@ for _ in 1:300
     @show cost
 end
 
-## Plot
+## Training v3
+for _ in 1:20
+    ode = NO.ODEStruct(ftrue,σ*randn32(2),tarray)
+    optim = Flux.Adam(1e-2)
+    cost = 0
+    for _ in 1:50
+        ufunc,cost = NO.node_train!(nn,ode,optim)
+    end
+    @show cost
+end
+
+## Plot savefig(joinpath(@__DIR__,"fig.png"))
 #index = 5
 u0 = σ*randn32(2)
-ode = NO.ODEStruct(ftrue,u0,tarray) #odelist[index]
-ufunc = NO.forward_pass(nn,ode.u0,(first(tarray),last(tarray)))# ufunclist[index]
+ode = NO.ODEStruct(ftrue,u0,tarray) 
+ufunc = NO.forward_pass(nn,ode.u0,(first(tarray),last(tarray)))
+soltrue1 = NO.ode_forward_pass(ftrue,u0,(first(tarray),last(tarray)))
 
-plot(ode.soltrue,label=["Ground truth u[1]" "Ground truth u[2]"])
-plot!(ufunc,label=["Prediction u[1]" "Prediction u[2]"],linestyle=:dash)
-#scatter!([u[1] for u in soltrue.u], [u[2] for u in soltrue.u], label="Observations")
+plot(soltrue1,label=["Ground truth u[1]" "Ground truth u[2]"],color=:green,dpi=600)
+plot!(ufunc,label=["Prediction u[1]" "Prediction u[2]"],linestyle=:dash,color=:blue)
+xlabel!("Time")
 
 ## Extrapolation
-tinf,tsup = -5f0,50f0
+tinf,tsup = -30f0,10f0
 
 tspan2 = (0.0f0, tsup)
 ufunc2 = NO.forward_pass(nn,u0,tspan2)
@@ -57,13 +75,14 @@ tspan3 = (0.0f0, tinf)
 ufunc3 = NO.forward_pass(nn,u0,tspan3)
 soltrue3 = NO.ode_forward_pass(ftrue,u0,tspan3)
 
-## Plot
-plot(soltrue3,label="Ground truth",color=:green,legend=false)
-plot!(ufunc3,label="Prediction",color=:blue,linestyle=:dash)
-plot!(soltrue2,label="Ground truth",color=:green)
-plot!(ufunc2,label="Prediction",color=:blue,linestyle=:dash)
+## Plot savefig(joinpath(@__DIR__,"fig.png"))
+plot(soltrue3,label=["Ground truth u[1]" "Ground truth u[2]"],color=:green,legend=true,dpi=600)
+plot!(ufunc3,label=["Prediction u[1]" "Prediction u[2]"],color=:blue,linestyle=:dash)
+plot!(soltrue2,label="",color=:green)
+plot!(ufunc2,label="",color=:blue,linestyle=:dash)
 xlims!(tinf,tsup)
 title!("Extrapolation")
+xlabel!("Time")
 
 ## Compare nn forward map angle
 t = range(0,1,100)
@@ -71,20 +90,23 @@ u = [Float32[cos(2π*t),sin(2π*t)] for t in t]
 dutrue = [A*u for u in u]
 dunn = [nn(u) for u in u]
 
-plot(t,[du[1] for du in dutrue],label="Ground truth u[1]",color=:green,legend=false)
+plot(t,[du[1] for du in dutrue],label="Ground truth u[1]",color=:green,legend=true,dpi=600)
 plot!(t,[du[2] for du in dutrue],label="Ground truth u[2]",color=:green)
 plot!(t,[du[1] for du in dunn],label="Prediction u[1]",color=:blue,linestyle=:dash)
 plot!(t,[du[2] for du in dunn],label="Prediction u[2]",color=:blue,linestyle=:dash)
+xlabel!("θ")
 
 ## Compare nn forward map radius
 t = range(0,1,100)
 r = 25
+rt = r*t
 ui = randn32(2)
 u = [r*ui*t for t in t]
 dutrue = [A*u for u in u]
 dunn = [nn(u) for u in u]
 
-plot(t,[du[1] for du in dutrue],label="Ground truth u[1]",color=:green,legend=false)
-plot!(t,[du[2] for du in dutrue],label="Ground truth u[2]",color=:green)
-plot!(t,[du[1] for du in dunn],label="Prediction u[1]",color=:blue,linestyle=:dash)
-plot!(t,[du[2] for du in dunn],label="Prediction u[2]",color=:blue,linestyle=:dash)
+plot(rt,[du[1] for du in dutrue],label="Ground truth u[1]",color=:green,legend=true,dpi=600)
+plot!(rt,[du[2] for du in dutrue],label="Ground truth u[2]",color=:green)
+plot!(rt,[du[1] for du in dunn],label="Prediction u[1]",color=:blue,linestyle=:dash)
+plot!(rt,[du[2] for du in dunn],label="Prediction u[2]",color=:blue,linestyle=:dash)
+xlabel!("r")
